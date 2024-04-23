3e30294555468422b134b624edaf852f1703423d
pedro-batista <peddrobatista2022@gmail.com>
2024-04-23 17:06:49 -0300

    Atualizando...

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const conectar = async () => {
-    if (global.conexao && global.conexao.this.state != 'disconected')
+    if (global.conexao && global.conexao.this.this.state != 'disconected')
         return global.conexao
     const mysql = require("mysql2/promise");
     const con = mysql.createConnection("mysql://root:@localhost:3306/nodeapi");
@@ -14,4 +14,24 @@ const todosClientes = async () => {
     return await linhas;
 }
 
-module.exports = {todosClientes};
+const insereClientes = async (cliente) => {
+    const con = await conectar();
+    const sql = 'INSERT INTO cliente_node (nome, idade) VALUES (?,?)';
+    const valores = [cliente.nome, cliente.idade];
+    await con.query(sql, valores);
+}
+
+const atualizaClientes = async (id, cliente) => {
+    const con = await conectar();
+    const sql = 'UPDATE cliente_node SET nome = ? , idade = ? WHERE id = ?';
+    const valores = [cliente.nome, cliente.idade, id];
+    await con.query(sql, valores);
+}
+
+const deletarClientes = async (id) => {
+    const con = await conectar();
+    const sql = 'DELETE FROM cliente_node WHERE id = ?';
+    const valores = [id];
+    await  con.query(sql, valores);
+}
+module.exports = {todosClientes, insereClientes, atualizaClientes, deletarClientes};
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 (async () => {
     const db = require('./db');
+    // console.log("Inserir novo cliente...");
+    // await db.insereClientes({nome: 'joão', idade: '22'})
+
+    /* await db.atualizaClientes(2, {nome: 'Carlos', idade: '27'})
+    console.log("Cliente atualizado...") */
+    const id = 4;
+    await db.deletarClientes(id);
+    console.log(`Cliente  de ID = ${id} foi deletado!`);
+
     console.log("Obtendo todos os clientes:");
     const clientes = await db.todosClientes();
     console.log(clientes);
